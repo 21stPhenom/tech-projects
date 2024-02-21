fd61d769a45c931f30e62c10076b1e6603b0fa17
21stPhenom <phenom.h.acks21@gmail.com>
2024-02-21 13:26:39 +0100

    update profile page in admin to include user's projects

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
 from accounts.models import Profile
+from projects.models import Project, Solution
 
 # Register your models here.
 class ProfileInline(admin.StackedInline):
@@ -14,8 +15,18 @@ class ProfileInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = [ProfileInline]
 
+class ProjectInline(admin.TabularInline):
+    model = Project
+    can_delete = False
+    verbose_name = 'project'
+    verbose_name_plural = 'projects'
+
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    inlines = (ProjectInline, )
+
 # Re-register User model to include Profiles in the admin page
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
