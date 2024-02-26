8f2222dc76aa3d24dd97c0172731c92139d6d4e6
21stPhenom <phenom.h.acks21@gmail.com>
2024-02-26 12:38:53 +0100

    update models

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from autoslug.fields import AutoSlugField
-
+from tinymce.models import HTMLField
 from accounts.models import Profile
 
 # Create your models here.
@@ -11,14 +11,16 @@ class Project(models.Model):
         '0': 'beginner',
         '1': 'intermediate',
         '2': 'advanced',
-        '3': 'pro'
     }
 
     creator = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='projects')
     name = models.CharField(verbose_name='Project Name', max_length=200, unique=True)
-    description = models.CharField(verbose_name='Project Description', max_length=1000)
+    short_description = models.CharField(verbose_name='Project Description', max_length=1000)
+    long_description = HTMLField(default='')
+    tags = ArrayField(
+        models.CharField(verbose_name='Project Tags', max_length=200, default='')
+    , default=list)
     enrolled_profiles = models.ManyToManyField(Profile, related_name='enrolled_profiles', blank=True)
-    reward = models.PositiveSmallIntegerField(verbose_name='Project Reward')
     difficulty = models.CharField(verbose_name='Project Difficulty', max_length=1, choices=difficulty_choices)
     project_slug = AutoSlugField(populate_from='name', unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -31,11 +33,12 @@ class Project(models.Model):
         return f'{self.name} by {self.creator.user.username}'
     
 class Solution(models.Model):
+    name = models.CharField(verbose_name='Solution Name', max_length=500, default='')
     uid = models.UUIDField(default=uuid.uuid4, editable=False)
     creator = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='solutions')
     project = models.OneToOneField(Project, on_delete=models.CASCADE, related_name='projects')
     repo_link = models.URLField(verbose_name='Solution Repository', max_length=500)
-    live_link = models.URLField(verbose_name='Live Solution', max_length=500)
+    live_link = models.URLField(verbose_name='Live Solution', max_length=500, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
